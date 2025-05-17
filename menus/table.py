19821c65d12ff2ce19af1b1aaf95c854a620c85b
Mateo Monsalve <thematius.monsalve@gmail.com>
2025-05-17 15:12:57 -0500

    Creación de menú principal (#30)
    * Init: Archivos de estructura ejemplo
    
    * New: Creating Main menu in a table, and general files
    
    ---------
    
    Co-authored-by: cddiazg <cristiandaviddiazgallego123@gmail.com>

---
+++ menus/table.py
@@ -0,0 +1,32 @@
+# Ansi colors for terminal output
+WHITE = "\033[97m"
+DARKCYAN = "\033[36m"
+BLUE = "\033[94m"
+YELLOW = "\033[93m"
+GREEN = "\033[92m"
+RED = "\033[91m"
+RESET = "\033[0m"
+
+# Function to display a table with a title and options
+def show_menu(options, title):
+    # Get the maximum width of the options
+    all_options = options + [title]
+    # Calculate the total width of the table
+    max_width = max([len(opt) for opt in all_options])
+    width = max_width + 4  # Add padding for the borders
+
+    # Print the table
+    print(f"{BLUE}╔{"═"*width}╗{RESET}")
+    print(f"{BLUE}║{RESET} {DARKCYAN}{title.center(width - 2)}{RESET} {BLUE}║{RESET}")
+    print(f"{BLUE}╠{"═"*width}╣{RESET}")
+    for i, option in enumerate(options):
+        print(f"{BLUE}║{RESET} {DARKCYAN}[{str(i + 1)}]{RESET} {WHITE}{option.ljust(width - 6)}{RESET} {BLUE}║{RESET}")
+    print(f"{BLUE}╚{"═"*width}╝{RESET}")
+    
+def show_error(error):   
+    width = len(error) + 30  # Add padding for the borders  
+
+    # Print the table
+    print(f"{RED}╔{"═"*width}╗{RESET}")
+    print(f"{RED}║{RESET} {RED}Error: {WHITE}{error.ljust(width - 9)}{RESET} {RED}║{RESET}")
+    print(f"{RED}╚{"═"*width}╝{RESET}")
