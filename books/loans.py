78063110f966e2e687dc09e4f0e278ec11e24a14
cddiazg <cristiandaviddiazgallego123@gmail.com>
2025-05-17 16:55:44 -0500

    FuncionValidarEstado (#31)

---
+++ books/loans.py
@@ -0,0 +1,16 @@
+def book_lend(books, id_book):
+    search_book = None
+
+    if id_book is None or len(id_book) == 0:
+        print("Error debes especificar un ID para buscar")
+        return search_book
+    
+    for book in books:
+        if id_book.lower() == book["id"].lower():
+            if book["Status"] == "Prestado":
+                return book["Loan"]
+            else: 
+                return {}
+
+    print(f"Error id no encontrado: {id_book}")
+    return search_book    
