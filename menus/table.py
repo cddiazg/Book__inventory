8ad629cf96f6b7ee061acd9cfd6104c34681702b
MatiusDev <mateomonsalve502@gmail.com>
2025-05-18 18:37:22 -0500

    New: Funcionalidad para imprimir mensajes en forma de taabla

--- menus/table.py
+++ menus/table.py
@@ -4,11 +4,12 @@ DARKCYAN = "\033[36m"
 BLUE = "\033[94m"
 YELLOW = "\033[93m"
 GREEN = "\033[92m"
+DARKGRAY = "\033[90m"
 RED = "\033[91m"
 RESET = "\033[0m"
 
 # Function to display a table with a title and options
-def show_menu(options, title):
+def show_menu(options: list, title: str):
     # Get the maximum width of the options
     all_options = options + [title]
     # Calculate the total width of the table
@@ -22,8 +23,16 @@ def show_menu(options, title):
     for i, option in enumerate(options):
         print(f"{BLUE}║{RESET} {DARKCYAN}[{str(i + 1)}]{RESET} {WHITE}{option.ljust(width - 6)}{RESET} {BLUE}║{RESET}")
     print(f"{BLUE}╚{"═"*width}╝{RESET}")
-    
-def show_error(error):   
+
+def show_info(info: str):
+    width = len(info) + 30  # Add padding for the borders  
+
+    # Print the table
+    print(f"{WHITE}╔{"═"*width}╗{RESET}")
+    print(f"{WHITE}║{RESET} {DARKGRAY}Info: {WHITE}{info.ljust(width - 8)}{RESET} {WHITE}║{RESET}")
+    print(f"{WHITE}╚{"═"*width}╝{RESET}")
+
+def show_error(error: str):
     width = len(error) + 30  # Add padding for the borders  
 
     # Print the table
