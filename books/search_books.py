05db108b15791ee0f236a82621e57f891e0f73ce
cristian_diaz <cristiandaviddiazgallego123@gmail.com>
2025-05-19 15:54:15 -0500

    Funcionalidad buscar libro por autor

--- books/search_books.py
+++ books/search_books.py
@@ -1,16 +1,25 @@
+import unicodedata
 from menus.table import show_error
+
 def search_author(books, author):
     search_author = None
+    author = author.lower().strip()
 
     if len(author.replace(" ", "")) == 0:
         show_error("debes especificar un author para buscar")
-
         return search_author
+    # Autor sin tildes
+    author_normalized = unicodedata.normalize('NFKD', author).encode('ascii','ignore').decode('utf-8')
+
     books_author = []
     for book in books:
-        if author.lower() == book["author"].lower() :
-            books_author.append(book)
-    if len(books_author) == 0:
+      book_normalized = unicodedata.normalize('NFKD', book["author"].lower()).encode('ascii', 'ignore').decode('utf-8')
+      if book_normalized.find(author_normalized) != -1:
+            books_author.append(book)  
+
+    if not books_author:
         show_error(f"author no encontrado: {author}")
-        return search_author        
+        return search_author
+
     return books_author
+
