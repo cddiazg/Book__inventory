4d5d096ccd873eb252118871b7b6a973b94c5d93
cristian_diaz <cristiandaviddiazgallego123@gmail.com>
2025-05-18 20:51:21 -0500

    SEARCH

--- books/search_books.py
+++ books/search_books.py
@@ -14,27 +14,3 @@ def search_author(books, author):
         show_error(f"author no encontrado: {author}")
         return search_author        
     return books_author
-
-# books = [
-#     { 
-#     "id" : 1,
-#     "title" : 'Cien años de soledad',
-#     "author" : 'Gabriel garcia marquez',
-#     "publication_year" : 2024,
-#     "category" : 'no ficcion',
-#     "status" : 'borrowed',
-#     "loan" : {"nombre": 'Daniel',"fecha_prestamo" : '2025-05-16'} 
-#     },
-#     { 
-#     "id" : 2,
-#     "title" : 'docientos años de soledad',
-#     "author" : 'Gabriel garcia marquez',
-#     "publication_year" : 2023   ,
-#     "category" : 'no ficcion',
-#     "status" : 'borrowed',
-#     "loan" : {"nombre": 'Daniel',"fecha_prestamo" : '2025-05-16'} 
-#     }
-# ]
-
-
-# print(search_author(books, " "))
