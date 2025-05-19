d4565a4ab26d4e782ac37ab05e93aabd6100c162
cristian_diaz <cristiandaviddiazgallego123@gmail.com>
2025-05-18 20:50:25 -0500

    SEARCH

--- books/search_books.py
+++ books/search_books.py
@@ -1,37 +1,40 @@
+from menus.table import show_error
 def search_author(books, author):
     search_author = None
 
-    if len(author.replace(" ", ""))== 0:
-        print("Error debes especificar un author para buscar")
+    if len(author.replace(" ", "")) == 0:
+        show_error("debes especificar un author para buscar")
+
         return search_author
-    
+    books_author = []
     for book in books:
         if author.lower() == book["author"].lower() :
-                return book["title"]
+            books_author.append(book)
+    if len(books_author) == 0:
+        show_error(f"author no encontrado: {author}")
+        return search_author        
+    return books_author
 
-    print(f"Error author no encontrado: {author}")
-    return search_author    
-
-books = [
-    { 
-    "id" : 1,
-    "title" : 'Cien años de soledad',
-    "author" : 'Gabriel garcia marquez',
-    "publication_year" : 2024,
-    "category" : 'no ficcion',
-    "status" : 'borrowed',
-    "loan" : {"nombre": 'Daniel',"fecha_prestamo" : '2025-05-16'} 
-    },
-    { 
-    "id" : 2,
-    "title" : 'docientos años de soledad',
-    "author" : 'Gabriel garcia marquez',
-    "publication_year" : 2023   ,
-    "category" : 'no ficcion',
-    "status" : 'borrowed',
-    "loan" : {"nombre": 'Daniel',"fecha_prestamo" : '2025-05-16'} 
-    }
-]
+# books = [
+#     { 
+#     "id" : 1,
+#     "title" : 'Cien años de soledad',
+#     "author" : 'Gabriel garcia marquez',
+#     "publication_year" : 2024,
+#     "category" : 'no ficcion',
+#     "status" : 'borrowed',
+#     "loan" : {"nombre": 'Daniel',"fecha_prestamo" : '2025-05-16'} 
+#     },
+#     { 
+#     "id" : 2,
+#     "title" : 'docientos años de soledad',
+#     "author" : 'Gabriel garcia marquez',
+#     "publication_year" : 2023   ,
+#     "category" : 'no ficcion',
+#     "status" : 'borrowed',
+#     "loan" : {"nombre": 'Daniel',"fecha_prestamo" : '2025-05-16'} 
+#     }
+# ]
 
 
-print(search_author(books, "gabriel garcia marquez"))
+# print(search_author(books, " "))
